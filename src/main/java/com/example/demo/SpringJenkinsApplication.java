6295297ccb2f4ebe0f8bd75ee689b8664e8c9840
sanjeev urmaliya <sanjeev.urmaliya89@gmai.com>
2022-02-25 10:35:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	
+	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	private void init() {
+		// TODO Auto-generated method stub
+       logger.info("init() : application started ");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		logger.info("main : Application executed ");
+	}
+
+}
---
+++ src/test/java/com/example/demo/SpringJenkinsApplicationTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class SpringJenkinsApplicationTest {
+private static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		logger.info("Test case are executing now ");
+		assertEquals(true, true);
+	}
+  
+	
+}
